99049e8fc288cdaca143e41a2cbe29d31b44b667
rgscherf <rgscherf@gmail.com>
2016-09-10 14:48:12 -0400

    range highlight and page layout

--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,7 @@ import Html.App as App
 import Math.Vector2 as V exposing (Vec2)
 import Cmd.Extra as C
 import Html
+import Html.Attributes as HA
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Svg.Events as SE
@@ -26,36 +27,59 @@ subscriptions _ =
         ]
 
 
+type alias Model =
+    { playerShip : Ship
+    , playerMovementRange : Float
+    , tileSize : Float
+    , boardSize : Float
+    , currentlyHighlightedTile : Vec2
+    , debugRangeToTile : ( Float, Float )
+    }
+
+
 init : Model
 init =
-    { ship = { pos = V.vec2 0 0 } }
+    { playerShip = Ship <| V.vec2 0 0
+    , playerMovementRange = 2
+    , tileSize = 30
+    , boardSize = 24
+    , currentlyHighlightedTile = V.vec2 0 0
+    , debugRangeToTile = ( 0, 0 )
+    }
 
 
 type alias Ship =
     { pos : Vec2 }
 
 
-type alias Model =
-    { ship : Ship }
-
-
 type Msg
     = NoOp
     | Move Ship Vec2
     | ClickOnTile Vec2
+    | CursorEnterTile Vec2
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         NoOp ->
-            model ! []
+            model
+                ! []
 
         Move s pt ->
-            ({ model | ship = { pos = pt } }) ! []
+            { model | playerShip = { pos = pt } }
+                ! []
 
         ClickOnTile vec ->
-            ( model, C.message <| Move model.ship vec )
+            model
+                ! [ C.message <| Move model.playerShip vec ]
+
+        CursorEnterTile vec ->
+            { model
+                | currentlyHighlightedTile = vec
+                , debugRangeToTile = ( V.distance model.playerShip.pos vec, model.tileSize * model.playerMovementRange )
+            }
+                ! []
 
 
 getShipPos : Ship -> ( Float, Float )
@@ -63,45 +87,68 @@ getShipPos { pos } =
     ( V.getX pos, V.getY pos )
 
 
-makeMapCoords : Float -> List ( Float, Float )
-makeMapCoords boardSize =
+makeMapCoords : Float -> Float -> List (Vec2)
+makeMapCoords boardSize tileSize =
     List.concat <|
         List.map
-            (\x -> List.map (\y -> ( x, y )) [0..boardSize])
+            (\x -> List.map (\y -> V.vec2 (x * tileSize) (y * tileSize)) [0..boardSize])
             [0..boardSize]
 
 
-makeMapTiles : Float -> Float -> List (Svg Msg)
-makeMapTiles boardSize tileSize =
+makeMapTiles : Model -> List (Svg Msg)
+makeMapTiles model =
     let
         boardCoords =
-            makeMapCoords boardSize
+            makeMapCoords model.boardSize model.tileSize
     in
-        List.map (makeOneRect tileSize) boardCoords
+        List.map (makeOneRect model) boardCoords
 
 
-makeOneRect : Float -> ( Float, Float ) -> Svg Msg
-makeOneRect tileSize ( x', y' ) =
+makeOneRect : Model -> Vec2 -> Svg Msg
+makeOneRect model pos =
     let
-        backgroundColor =
-            "AliceBlue"
+        pos' =
+            V.toRecord pos
 
-        x'' =
-            x' * tileSize
+        notHighlightedColor =
+            "RoyalBlue"
 
-        y'' =
-            y' * tileSize
+        notHighligtedButInRange =
+            "Blue"
+
+        highlightedInRangeColor =
+            "Salmon"
+
+        highligtedOutOfRangeColor =
+            "LightSlateGrey"
+
+        isInRange =
+            V.distance model.playerShip.pos pos <= (model.tileSize * model.playerMovementRange)
+
+        isHighligtedTile =
+            model.currentlyHighlightedTile == pos
+
+        tileColor =
+            if isHighligtedTile then
+                if isInRange then
+                    highlightedInRangeColor
+                else
+                    highligtedOutOfRangeColor
+            else if isInRange then
+                notHighligtedButInRange
+            else
+                notHighlightedColor
     in
         rect
-            [ x <| toString x''
-            , y <| toString y''
-            , fill backgroundColor
-            , width <| toString tileSize
-            , height <| toString tileSize
-            , stroke "LavenderBlush"
+            [ x <| toString pos'.x
+            , y <| toString pos'.y
+            , fill <| tileColor
+            , width <| toString model.tileSize
+            , height <| toString model.tileSize
+            , stroke "Teal"
             , strokeWidth "1px"
-            , SE.onClick <| ClickOnTile (V.vec2 x'' y'')
-            , Se.onHover <| 
+            , SE.onClick <| ClickOnTile pos
+            , SE.onMouseOver <| CursorEnterTile pos
             ]
             []
 
@@ -109,25 +156,31 @@ makeOneRect tileSize ( x', y' ) =
 view : Model -> Svg Msg
 view model =
     let
-        boardSize =
-            10
-
-        tileSize =
-            50
-
         gameSizeStr =
-            toString <| boardSize * tileSize
+            toString <| model.boardSize * model.tileSize
     in
         Html.div []
-            [ Svg.svg [ width gameSizeStr, height gameSizeStr ] <|
-                (makeMapTiles boardSize tileSize)
-                    ++ [ rect
-                            [ x (toString <| fst <| getShipPos model.ship)
-                            , y (toString <| snd <| getShipPos model.ship)
-                            , fill "LightPink"
-                            , width <| toString tileSize
-                            , height <| toString tileSize
-                            ]
-                            []
-                       ]
+            [ Html.div [ HA.style [ ( "display", "flex" ), ( "flex-direction", "row" ) ] ]
+                [ Svg.svg
+                    [ width gameSizeStr, height gameSizeStr ]
+                  <|
+                    List.append (makeMapTiles model) (renderPlayer model)
+                , Html.div
+                    [ HA.style [ ( "width", "400px" ), ( "background", "LightSlateGrey" ) ] ]
+                    [ text "Hello world" ]
+                ]
+            , Html.div [] [ text <| toString model ]
             ]
+
+
+renderPlayer : Model -> List (Svg a)
+renderPlayer model =
+    [ rect
+        [ x (toString <| fst <| getShipPos model.playerShip)
+        , y (toString <| snd <| getShipPos model.playerShip)
+        , fill "Aquamarine"
+        , width <| toString model.tileSize
+        , height <| toString model.tileSize
+        ]
+        []
+    ]
