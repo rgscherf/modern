dd266e501d508c2813201aa7b6beab7fa7a410d4
rgscherf <rgscherf@gmail.com>
2016-09-11 12:20:23 -0400

    enemy spawn and mouseover info

--- src/Main.elm
+++ src/Main.elm
@@ -3,13 +3,14 @@ module Main exposing (..)
 import Html.App as App
 import Math.Vector2 as V exposing (Vec2)
 import Cmd.Extra as C
-import Html
+import Html exposing (Html)
 import Html.Attributes as HA
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Svg.Events as SE
 import Keyboard
 import Char
+import Random exposing (Seed)
 
 
 --------
@@ -25,11 +26,17 @@ type alias Model =
     , currentlyHighlightedTile : Vec2
     , enemies : List Ship
     , enemySpawnCountdown : Int
+    , randomSeed : Seed
+    , timeBetweenEnemySpawns : Int
     }
 
 
-init : Model
-init =
+type alias InitFlags =
+    { startTime : Int }
+
+
+init : InitFlags -> ( Model, Cmd Msg )
+init { startTime } =
     { playerShip = Ship <| V.vec2 0 0
     , playerMovementRange = 2
     , tileSize = 30
@@ -37,11 +44,20 @@ init =
     , currentlyHighlightedTile = V.vec2 0 0
     , enemies = []
     , enemySpawnCountdown = 3
+    , randomSeed = Random.initialSeed startTime
+    , timeBetweenEnemySpawns = 3
     }
+        ! []
 
 
 type alias Ship =
     { pos : Vec2 }
+
+
+type TerrainType
+    = Sea
+    | Player
+    | Enemy
 
 
 type Msg
@@ -50,12 +66,56 @@ type Msg
     | ClickOnTile Vec2
     | CursorEnterTile Vec2
     | KeyboardEvent Char
+    | SpawnEnemy
 
 
 
 ---------
 -- UPDATE
 ---------
+
+
+(!!) : List a -> Int -> Maybe a
+(!!) l ind =
+    let
+        go l' ind' =
+            if ind' <= 0 then
+                List.head l'
+            else
+                go (Maybe.withDefault [] (List.tail l')) (ind' - 1)
+    in
+        go l ind
+infixr 1 !!
+
+
+spawnNewEnemy : Model -> ( Ship, Seed )
+spawnNewEnemy model =
+    let
+        allCoords =
+            makeMapCoords model.boardSize model.tileSize
+
+        allFreeCoords =
+            List.filter (tileIsNotBlocked model) allCoords
+
+        generator =
+            Random.int 0 ((List.length allFreeCoords) - 1)
+
+        ( pickedIndex, newSeed ) =
+            Random.step generator model.randomSeed
+
+        newShipPos =
+            Maybe.withDefault (V.vec2 0 0) (allFreeCoords !! pickedIndex)
+    in
+        ( { pos = newShipPos }, newSeed )
+
+
+tileIsNotBlocked : Model -> Vec2 -> Bool
+tileIsNotBlocked model vec =
+    let
+        entityPositions =
+            model.playerShip.pos :: (List.map .pos model.enemies)
+    in
+        not <| List.member vec entityPositions
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -82,19 +142,34 @@ update msg model =
                         >= 0
                         && newY
                         < (model.tileSize * model.boardSize)
+
+                newVec =
+                    V.vec2 newX newY
             in
-                if moveIsInBounds then
+                if moveIsInBounds && tileIsNotBlocked model newVec then
                     { model
-                        | playerShip = { pos = V.vec2 newX newY }
+                        | playerShip = { pos = newVec }
                         , enemySpawnCountdown = model.enemySpawnCountdown - 1
                     }
                         ! (if model.enemySpawnCountdown == 0 then
-                            []
+                            [ C.message SpawnEnemy ]
                            else
                             []
                           )
                 else
                     model ! []
+
+        SpawnEnemy ->
+            let
+                ( newShip, newSeed ) =
+                    spawnNewEnemy model
+            in
+                { model
+                    | enemySpawnCountdown = model.timeBetweenEnemySpawns
+                    , enemies = newShip :: model.enemies
+                    , randomSeed = newSeed
+                }
+                    ! []
 
         ClickOnTile vec ->
             model
@@ -144,24 +219,77 @@ update msg model =
 -------
 
 
+queryMouseHighlight : Model -> Vec2 -> TerrainType
+queryMouseHighlight model vec =
+    if vec == model.playerShip.pos then
+        Player
+    else if List.member vec (List.map .pos model.enemies) then
+        Enemy
+    else
+        Sea
+
+
+type alias MouseInfoDivPackage =
+    { headline : String
+    , description : String
+    }
+
+
+fillMouseInfoDiv : TerrainType -> MouseInfoDivPackage
+fillMouseInfoDiv terr =
+    case terr of
+        Player ->
+            MouseInfoDivPackage "It's you!" "The intrepid trader"
+
+        Enemy ->
+            MouseInfoDivPackage "A pirate" "Run or fight"
+
+        Sea ->
+            MouseInfoDivPackage "Open sea" "Nothing to see..."
+
+
 view : Model -> Svg Msg
 view model =
     let
         gameSizeStr =
             toString <| model.boardSize * model.tileSize
+
+        mouseDivPackage =
+            fillMouseInfoDiv <| queryMouseHighlight model model.currentlyHighlightedTile
     in
         Html.div []
             [ Html.div [ HA.style [ ( "display", "flex" ), ( "flex-direction", "row" ) ] ]
                 [ Svg.svg
                     [ width gameSizeStr, height gameSizeStr ]
                   <|
-                    List.append (makeMapTiles model) (renderPlayer model)
+                    (makeMapTiles model)
+                        ++ (List.map (renderEnemy model.tileSize) model.enemies)
+                        ++ (renderPlayer model)
                 , Html.div
                     [ HA.style [ ( "width", "400px" ), ( "background", "LightSlateGrey" ) ] ]
-                    [ text "Hello world" ]
+                    [ Html.h1 [] [ text <| mouseDivPackage.headline ]
+                    , Html.h2 [] [ text <| mouseDivPackage.description ]
+                    ]
                 ]
             , Html.div [] [ text <| toString model ]
             ]
+
+
+renderEnemy : Float -> Ship -> Svg Msg
+renderEnemy tileSize { pos } =
+    let
+        pos' =
+            V.toRecord pos
+    in
+        rect
+            [ x <| toString pos'.x
+            , y <| toString pos'.y
+            , fill <| "Red"
+            , width <| toString tileSize
+            , height <| toString tileSize
+            , SE.onMouseOver <| CursorEnterTile pos
+            ]
+            []
 
 
 makeMapCoords : Float -> Float -> List (Vec2)
@@ -224,7 +352,6 @@ makeOneRect model pos =
             , fill <| tileColor
             , width <| toString model.tileSize
             , height <| toString model.tileSize
-            , SE.onClick <| ClickOnTile pos
             , SE.onMouseOver <| CursorEnterTile pos
             ]
             []
@@ -235,7 +362,7 @@ getShipPos { pos } =
     ( V.getX pos, V.getY pos )
 
 
-renderPlayer : Model -> List (Svg a)
+renderPlayer : Model -> List (Svg Msg)
 renderPlayer model =
     [ rect
         [ x (toString <| fst <| getShipPos model.playerShip)
@@ -243,6 +370,7 @@ renderPlayer model =
         , fill "Aquamarine"
         , width <| toString model.tileSize
         , height <| toString model.tileSize
+        , SE.onMouseOver <| CursorEnterTile model.playerShip.pos
         ]
         []
     ]
@@ -254,10 +382,10 @@ renderPlayer model =
 -------
 
 
-main : Program Never
+main : Program { startTime : Int }
 main =
-    App.program
-        { init = ( init, Cmd.none )
+    App.programWithFlags
+        { init = init
         , update = update
         , subscriptions = subscriptions
         , view = view
